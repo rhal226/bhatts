611d580b5e8dbb7c262b77c2aa7c7ab27cb79552
psbhatt <praneethsbhatt@gmail.com>
2023-04-22 12:37:50 -0400

    sorts datasets, optimizes backend filtering, and attempts to implement api connection

--- Backend/backend.py
+++ Backend/backend.py
@@ -11,11 +11,23 @@ Usage:
 '''
 
 
-
+from flask import Flask, request
 import docopt
 import functools
 import pandas as pd
 import numpy as np
+import json
+
+
+app = Flask(__name__)
+
+
+def fixOrder():
+    df = pd.read_csv("../data/disease_precautions.csv")
+    print(df)
+    df = df.sort_values("Disease").reset_index(drop=True)
+    print(df)
+    df.to_csv("../data/disease_precautions.csv", index=False)
 
 
 def searchSymp(symptoms):
@@ -42,10 +54,42 @@ def searchDisease(disease):
     return result.to_json
 
 
+@app.route('/SearchDatabase/<string:arg>', methods=["GET"])
+def searchDB(arg):
+    arg = json.loads(arg)
+    descDF = pd.read_csv("../data/disease_description.csv")
+    sympDF = pd.read_csv("../data/disease_symptoms.csv")
+    precDF = pd.read_csv("../data/disease_precautions.csv")
+    mask = None
+
+    if arg["symptom"]:
+        mask = functools.reduce(np.logical_or, [sympDF[f"Symptom_{i}"].str.contains(arg["<symptom>"][0], case=False) for i in
+                                                range(1, 18)]).fillna(False)
+    elif arg["disease"]:
+        mask = functools.reduce(np.logical_or, [descDF["Disease"].str.contains(arg["<disease>"][0], case=False)])
+
+    descDF = descDF[mask]
+    sympDF = sympDF[mask].dropna(axis=1)
+    precDF = precDF[mask].dropna(axis=1)
+
+    print(descDF)
+    print(sympDF)
+    print(precDF)
+
+    return [descDF, sympDF, precDF]
+
+
+
+
+
+
 if __name__ == '__main__':
-    args = docopt.docopt(__doc__)
+    # args = docopt.docopt(__doc__)
     # print(args)
-    if args["symptom"]:
-        searchSymp(args["<symptom>"])
-    elif args["disease"]:
-        searchDisease(args["<disease>"])
+    # if args["symptom"]:
+    #     searchSymp(args["<symptom>"])
+    # elif args["disease"]:
+    #     searchDisease(args["<disease>"])
+    # fixOrder()
+    # searchDB(args)
+    app.run(debug=True)
